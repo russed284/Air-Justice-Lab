3ce1b2774f875843240a1216eceafdd6d97d88af
russed284 <112986318+russed284@users.noreply.github.com>
2026-02-26 12:47:32 -0500

    Add files via upload

---
+++ src/app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 25 00:15:41 2026
+
+@author: daltonrussell
+"""
+
+import dash
+from dash import dcc, html, Input, Output, callback
+import dash_bootstrap_components as dbc
+import folium
+from folium.plugins import HeatMap
+import pandas as pd
+import base64
+import io
+from datetime import date
+import numpy as np
+import os
+from components.navbar import get_navbar
+
+app = dash.Dash(
+    __name__,
+    use_pages=True,
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    suppress_callback_exceptions=True,
+    title='Air Justice Dash App'
+    )
+##print("Inside app:", dash.page_registry.values())
+
+def serve_layout():
+    
+    return html.Div(
+        [
+            
+            get_navbar(),
+            dbc.Container(
+                dash.page_container,
+                class_name='my-2'      
+                )
+            
+            ]
+        
+        )
+
+app.layout = serve_layout
+server = app.server
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8092)
